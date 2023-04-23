2fbaad4b212fff5d48e9196670f2c4c37715826d
Alex Dikov <dikov_alex@mail.ru>
2023-04-23 21:45:14 +0300

    сверстал футер

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,14 @@ export default function Footer() {
   return (
     <>
       <footer className="footer">
-        <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
+        <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+        <div className="footer__container">
+          <p>&copy; 2023</p>
+          <div style={{display:"flex"}}>
+            <p>Яндекс.Практикум</p>
+            <p style={{marginLeft:"20px"}}>GitHub</p>
+          </div>
+        </div>
       </footer>
     </>
   );
